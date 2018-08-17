53d1d5842d68f97887f21de0ff7127b37038f5d0
torrent benoit <torrentdu64@gmail.com>
2018-08-17 16:13:53 +1200

    create reducer weather with ...state for flat data in to array

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,15 +1,14 @@
-import axios from 'axios';
+import axios from "axios";
 
 const API_KEY = "c632991d3940903768ddbe468969a101";
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
-const BASE_URL = `https://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+export const FETCH_WEATHER = "FETCH_WEATHER";
 
-export const FETCH_WEATHER = 'FETCH_WEATHER';
-
-
-function fetchWeather(city){
-  const url = `${BASE_URL}&q=${city},nz`;
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
   const promise = axios.get(url);
+
   return {
     type: FETCH_WEATHER,
     payload: promise
