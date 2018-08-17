351a9c8a477453b4067a0fedeb756d5ddf5f7bf4
torrent benoit <torrentdu64@gmail.com>
2018-08-17 15:11:35 +1200

    create action for fetch api data city

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,17 @@
-
+import axios from 'axios';
 
 const API_KEY = "c632991d3940903768ddbe468969a101";
 
+const BASE_URL = `https://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+
+function fetchWeather(city){
+  const url = `${BASE_URL}&q=${city},nz`;
+  const promise = axios.get(url);
+  return {
+    type: FETCH_WEATHER,
+    payload: promise
+  };
+}
